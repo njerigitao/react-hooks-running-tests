bae1782e4617098bfbb79bc50661b87735b105a8
AGNESGITAU <agnesgitau40@gmail.com>
2024-04-22 15:00:21 +0300

    debugging-tools

--- src/__tests__/Article.test.js
+++ src/__tests__/Article.test.js
@@ -6,7 +6,8 @@ import Article from "../components/Article";
 
 test("displays the text 'please pass this test'", () => {
   render(<Article />);
-  screen.debug();
+  const element = screen.queryByText("please pass this test");
+  screen.debug(element);
 
-  expect(screen.queryByText("please pass this test")).toBeInTheDocument();
+  expect(element).toBeInTheDocument();
 });
